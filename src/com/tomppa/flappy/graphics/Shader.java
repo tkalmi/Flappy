6bfa45c2a89eee54d6178d91c946e537c107ca31
tkalmi <tomi.kalmi@aalto.fi>
2015-10-13 22:05:50 +0100

    Implemented shader

---
+++ src/com/tomppa/flappy/graphics/Shader.java
@@ -0,0 +1,67 @@
+package com.tomppa.flappy.graphics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.tomppa.flappy.maths.Matrix4f;
+import com.tomppa.flappy.maths.Vector3f;
+import com.tomppa.flappy.utils.ShaderUtils;
+
+import static org.lwjgl.opengl.GL20.*;
+
+
+public class Shader {
+	/*
+	 * Point is to create class that calls OpenGL --> We don't have to write OpenGL code everytime we want to use it
+	 */
+	
+	private final int ID;
+	private Map<String, Integer> locationCache = new HashMap<String, Integer>();
+	
+	public Shader(String vertex, String fragment) {
+		ID = ShaderUtils.load(vertex, fragment);
+	}
+	
+	public int getUniform(String name) {
+		if (locationCache.containsKey(name)) {
+			return locationCache.get(name);
+		}
+		
+		int result = glGetUniformLocation(ID, name);
+		if (result == -1) {
+			System.err.println("Could not find uniform variable '" + name + "'!");
+		} else {
+			locationCache.put(name, result);
+		}
+		return result;
+	}
+	
+	public void setUniform1i(String name, int value) {
+		glUniform1i(getUniform(name), value);
+	}
+
+	public void setUniform1f(String name, float value) {
+		glUniform1f(getUniform(name), value);
+	}
+
+	public void setUniform2f(String name, float x, float y) {
+		glUniform2f(getUniform(name), x, y);
+	}
+
+	public void setUniform3f(String name, Vector3f vector) {
+		glUniform3f(getUniform(name), vector.x, vector.y, vector.z);
+	}
+	
+	public void setUniformMat4f(String name, Matrix4f matrix) {
+		glUniformMatrix4fv(getUniform(name), false, matrix.toFloatBuffer()); // HUOM!! Eroaa videon versiosta!?!?!?
+	}
+	
+	public void enable() {
+		glUseProgram(ID);
+	}
+	
+	public void disable() {
+		glUseProgram(0);
+	}
+
+}
---
+++ src/com/tomppa/flappy/utils/ShaderUtils.java
@@ -0,0 +1,54 @@
+package com.tomppa.flappy.utils;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+
+public class ShaderUtils {
+	/*
+	 * Loads a file and returns an OpenGL ID
+	 */
+	
+	private ShaderUtils() {
+		
+	}
+	
+	public static int load(String vertPath, String fragPath) {
+		String vert = FileUtils.loadAsString(vertPath);
+		String frag = FileUtils.loadAsString(vertPath);
+		return create(vert, frag);
+	}
+	
+	public static int create(String vert, String frag) {
+		int program = glCreateProgram();
+		int vertID = glCreateShader(GL_VERTEX_SHADER);
+		int fragID = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(vertID, vert);
+		glShaderSource(fragID, frag);
+
+		glCompileShader(vertID);
+		if (glGetShaderi(vertID, GL_COMPILE_STATUS) == GL_FALSE) {
+			System.err.println("Failed to compile vertex shader!");
+			System.err.println(glGetShaderInfoLog(vertID));
+			return -1;
+		}
+		
+		glCompileShader(fragID);
+		if (glGetShaderi(fragID, GL_COMPILE_STATUS) == GL_FALSE) {
+			System.err.println("Failed to compile fragment shader!");
+			System.err.println(glGetShaderInfoLog(fragID));
+			return -1;
+		}
+		
+		glAttachShader(program, vertID);
+		glAttachShader(program, fragID);
+		glLinkProgram(program);
+		glValidateProgram(program);
+		
+		glDeleteShader(vertID);
+		glDeleteShader(fragID);
+		
+		return program;
+	}
+	
+	
+}
